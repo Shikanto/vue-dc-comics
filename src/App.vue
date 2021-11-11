74e4528fffa9a6b1681af8e0a8e1c65038f3cdc7
Shikanto <antonello.anzini.comunicazioni@gmail.com>
2021-11-11 16:13:53 +0100

    create files

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Header></Header>
+    <Main></Main>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+//qui importiamo i componenti
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Header,
+    Main,
+    Footer
   },
 };
 </script>
 
 <style lang="scss">
-#app {
+
+ #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
